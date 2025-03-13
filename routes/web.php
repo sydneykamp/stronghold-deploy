d56fe0c15a1493447962f71c7b3e8b40192bea24
Sydney <SydneyKampjes@gmail.com>
2025-03-13 16:41:14 -0600

    trying

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\MemorialController;
 use App\Http\Controllers\TestimonialVideoController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/{any}', function () {
-    return view('home');
-})->where('any', '.*');
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', function () {
