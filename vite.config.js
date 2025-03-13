01656f51d589ff5037a1b8f5e89c7802263bba11
Sydney <SydneyKampjes@gmail.com>
2025-03-13 14:44:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/styles.css', 'resources/js/main.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/styles.css', 'resources/js/main.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build'
+  }
 });
